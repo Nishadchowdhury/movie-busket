c518e0848d6ce77182ea837d349139d30ad538ba
Nishadchowdhury <Nishadhj111@gmail.com>
2024-05-28 16:09:35 +0600

    first commit of the movie app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-center text-red-700 container oldstyle-nums ' >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, illo.
+      </h1>
+    </>
+  )
+}
+
+export default App
