aae31abae94ed2a2349156b543876e7736720b47
Nishadchowdhury <Nishadhj111@gmail.com>
2024-05-31 11:55:43 +0600

    context api added and two modal added cart and movie card

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
 
+import { useState } from 'react'
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import MovieList from './components/MovieList'
 import Sidebar from './components/Sidebar'
+import { MovieContext } from './contexts'
 
 function App() {
 
+  const [cartData, setCartData] = useState([])
+
+
   return (
     <>
-      <Header />
-      <main>
-        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-          <Sidebar />
-          <MovieList />
-        </div>
-      </main>
-      <Footer />
+      <MovieContext.Provider value={{ cartData, setCartData }}>
+        <Header />
+        <main>
+          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+            <Sidebar />
+            <MovieList />
+          </div>
+        </main>
+        <Footer />
+      </MovieContext.Provider>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react"
+
 import logo from "../assets/logo.svg"
 import ring from "../assets/ring.svg"
 import icons from "../assets/icons/sun.svg"
 import shoppingCart from "../assets/shopping-cart.svg"
+import CartModal from "./CartModal"
 
 export default function Header() {
+
+    const [showModal, setShowModal] = useState(false);
+
+
+    function handleCartShow() {
+        setShowModal(true)
+    }
+
+    function handleCartHide() {
+        setShowModal(false)
+    }
+
     return (
 
         <>
+
             <header>
+
+                {showModal && <CartModal close={handleCartHide} />}
+
                 <nav className="container flex items-center justify-between space-x-10 py-6">
                     <a href="index.html">
                         <img
@@ -34,7 +53,7 @@ export default function Header() {
                             </a>
                         </li>
                         <li>
-                            <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                            <a onClick={handleCartShow} className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
                                 <img
                                     src={shoppingCart}
 
